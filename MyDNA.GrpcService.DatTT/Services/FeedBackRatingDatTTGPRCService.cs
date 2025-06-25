8330ba94214e126bb7ab031bd76343af5518f7cf
MotUika <motdapchai@gmail.com>
2025-06-25 09:06:37 +0700

    update getall protos

---
+++ MyDNA.GrpcService.DatTT/Services/FeedBackRatingDatTTGPRCService.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Core;
+using MyDNA.GrpcService.DatTT.Protos;
+using MyDNA.Repositories.DatTT.Models;
+using MyDNA.Services.DatTT;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MyDNA.GrpcService.DatTT.Services
+{
+    public class FeedBackRatingDatTTGPRCService : FeedBackRatingDatTTGRPC.FeedBackRatingDatTTGRPCBase
+    {
+        private readonly IServiceProviders _serviceProviders;
+        public FeedBackRatingDatTTGPRCService(IServiceProviders serviceProviders) => _serviceProviders = serviceProviders;
+        public override async Task<FeedBackRatingResponseList> GetAllAsync(EmptyRequest request, ServerCallContext context)
+        {
+            var result = new FeedBackRatingResponseList();
+
+            try
+            {
+                var feedBackRatings = await _serviceProviders.FeedBackRatingDatTTService.GetAllAsync();
+
+                var opt = new JsonSerializerOptions() { ReferenceHandler = ReferenceHandler.IgnoreCycles, DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull };
+
+
+                var feedBackRatingJsonString = JsonSerializer.Serialize(feedBackRatings, opt);
+
+
+                var items = JsonSerializer.Deserialize<List<FeedBackRatingResponse>>(feedBackRatingJsonString, opt);
+
+
+                result.Items.AddRange(items);
+            }
+            catch (Exception ex) { }
+
+            return result;
+        }
+    }
+}

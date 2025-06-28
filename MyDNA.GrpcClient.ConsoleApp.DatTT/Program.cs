4d34de9070da0453303905cd369ce0b57bfef6d5
MotUika <motdapchai@gmail.com>
2025-06-28 09:13:23 +0700

    Update create update delete for client

--- MyDNA.GrpcClient.ConsoleApp.DatTT/Program.cs
+++ MyDNA.GrpcClient.ConsoleApp.DatTT/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Grpc.Net.Client;
-using MyDNA.GrpcService.DatTT.Protos;
+using MyDNA.GrpcClient.ConsoleApp.DatTT.Protos;
 
 using var channel = GrpcChannel.ForAddress("https://localhost:7244");
 
@@ -17,5 +17,36 @@ if (feedBackRating != null && feedBackRating.Items.Count > 0)
 }
 
 Console.WriteLine("\r\nclient.GetByIdAsync(FeedBackRatingRequest={1}):");
-var one = clientFeedBackGRPC.GetByIdAsync(new FeedBackRatingRequest { FeedBackRatingDatTtid = 3 });
+var one = clientFeedBackGRPC.GetByIdAsync(new FeedBackRatingRequest { FeedBackRatingDatTtid =  });
 Console.WriteLine($"{one.FeedBackRatingDatTtid} - {one.WriterName}");
+
+var createResponse = clientFeedBackGRPC.CreateAsync(new FeedBackRatingRequestUpdate
+{
+    ServiceDatTtid = 1,
+    Rating = 5,
+    WriterName = "John Doe",
+    Title = "Excellent!",
+    Content = "Very satisfied.",
+    IsVisible = true,
+    UpdatedAt = DateTime.UtcNow.ToString("o"),
+    FeedbackScore = 100
+});
+Console.WriteLine($"\nCreateAsync: Result = {createResponse.Result}");
+
+var updateResponse = clientFeedBackGRPC.UpdateAsync(new FeedBackRatingRequestUpdate
+{
+    FeedBackRatingDatTtid = 23,
+    ServiceDatTtid = 1,
+    Rating = 4,
+    WriterName = "John Updated",
+    Title = "Still Good",
+    Content = "A bit slower, but okay.",
+    IsVisible = true,
+    UpdatedAt = DateTime.UtcNow.ToString("o"),
+    FeedbackScore = 85
+});
+Console.WriteLine($"\nUpdateAsync: Result = {updateResponse.Result}");
+
+// DELETE
+var deleteResponse = clientFeedBackGRPC.DeleteAsync(new FeedBackRatingRequest { FeedBackRatingDatTtid = 1 });
+Console.WriteLine($"\nDeleteAsync: Result = {deleteResponse.Result}");
